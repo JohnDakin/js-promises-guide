aab3b96929642d9edcbe90825f941a70ffcbefe7
John Dakin <jdakin749@gmail.com>
2026-07-28 15:54:04 +0300

    Add JavaScript Promises guide with examples and explanatory comments

---
+++ index.js
@@ -0,0 +1,314 @@
+// Promises
+// state -  
+      // Pending : Initially when the executor function starts the execution 
+      // fulfilled : when the promise is resolved.
+      // rejected : when the promise is rejected.
+
+//result - 
+        //undefined : Initially when the state is pending.
+        //value : When resolve(value) is called.
+        //error : when reject(error) is called.
+
+
+// when promise - is created for the first time it state will be pending
+/*
+let promise = new Promise(function(resolve, reject){
+
+});
+*/
+
+//Executor function - handles the asynchronous operation
+// resolve and reject - both are callback functions provided by javascript
+/*
+function(resolve, reject){
+  // Logic goes here
+  //you won't be calling both resolve and reject in subsequent manner maybe either one of them 
+  // but you may conditionally call both resolve or reject
+}
+*/
+
+// How promises are resolved and rejected
+/*
+let promise1 = new Promise(function(resolve, reject){
+  resolve("Hey, I am done!");
+})
+
+
+let promise2 = new Promise(function(resolve, reject){
+  reject("Something is not right");
+})
+
+let anotherPromise = new Promise(function(resolve, reject){
+  resolve("I am surely going to get resolved!");
+
+  reject(new Error("Will this be ignored?")); //ignored
+  resolve("Ignored") // ignored
+})
+*/
+//  Handling Promises
+//Handler methods - .then() .catch() .finall() - helps to create the link between the service provider/Executor and the customer/client
+
+// .then() - accepts two parameter 1st resolve and 2nd reject
+/*
+let loading = false;
+const promise = new Promise(function(resolve, reject){
+  loading = true;
+  // Make a Network Call(API Call/IO Operation)
+  resolve("I am resolved...");
+});
+
+promise.then(
+  //if your intrested for the successful outcome you can pass only one argument and ignore the other 
+  // if you remove the result handler just pass a null
+  // (result)=>{console.log(result)},
+  // null,
+  (result)=>{console.log(result)},
+  // (error)=>{console.error(error)}
+).catch(
+  (error) => {console.error(error)}
+).finally(
+  () => {
+    loading = false;
+  }
+);
+*/
+// Promise Chain
+
+// Rule 1: Every promise gives you a .then() handler method. Every rejected promise provides a .catch() handler.
+
+// Rule 2: You can do mainly three valuable things from the .then() method. You can return another promise(for async operation). You can return any other value from a synchronous operation. Lastly, you can throw an error.
+
+//---------------------------------------------------------------------------------------------------------------------------------------------
+//Return a promise from the .then() handler
+
+/*
+
+//Create a Promise
+let getUser = new Promise(function(resolve, reject){
+  const user = {
+    name: 'John Doe',
+    email: 'jdoe@email.com',
+    password: 'jdoe.password'
+  };
+  resolve(user);
+})
+
+getUser.then(function(user){
+
+  console.log(`Got user ${user.name}`);
+
+  return new Promise(function(resolve, reject){
+    setTimeout(function(){
+      resolve('Bangalore');
+    }, 2000)
+  })
+})
+.then((address)=>{
+  console.log(`User address is ${address}`);
+})
+
+// Return a simple value from the .then() handler
+
+getUser.then(function(user){
+  console.log(`Got user ${user.name}`);
+  return user.email;
+})
+.then(function(email){
+  console.log(`User email is ${email}`);
+})
+*/
+
+// Throw an error from the .then() handler
+
+/*
+let getUser = new Promise(function(resolve, reject){
+  const user = {
+    name: 'John Doe',
+    email: 'jdoe@email.com',
+    password: 'jdoe.password',
+    permisions: ['db', 'dev']
+  };
+  resolve(user);
+})
+
+getUser
+.then((user)=>{
+  console.log(`Got user ${user.name}`);
+
+  if(!user.permisions.includes("hr")){
+    throw new Error("You are not allowed to access the HR module");
+  }
+
+  return user.email;
+})
+.then((email) => {
+  console.log(`User email is ${email}`)
+})
+.catch((error)=>{
+  console.error(error);
+})
+*/
+// Rule 3: You can rethrow from the .catch() handler to handle the error later. In this case, the control will go to the next closest .catch() handler.
+/*
+let promise401 = new Promise(function(resolve, reject){
+  reject(401)
+});
+
+promise401.catch((error) =>{
+  console.log(error);
+  if(error === 401){
+    console.log("Rethrowing 401");
+    throw error;
+  }else{
+    //Do Something
+  }
+  })
+
+.then((result)=> {
+  console.log(result);
+})
+
+.catch((error) => {
+  console.log(`handling ${error} here`);
+})
+*/
+
+//Rule 4 - Unlike .then() and .catch(), the .finally() handler doesn't process the result value or error. It just passes the result as is to the next handler.
+/*
+let promiseFinally = new Promise(function(resolve, reject){
+  resolve('Testing Finally.');
+});
+
+promiseFinally.finally(function(){
+  console.log("Running Finally!");
+})
+.then(function(result){
+  console.log(result)
+})
+*/
+//Rule 5 - Calling the .then() handler method multiple times on a single promise is NOT chaining.
+
+/*
+//Not Chaining
+promise
+  .then((result)=>{
+    //Do Something
+    return 101;
+  })
+promise
+  .then((result)=>{
+    //result // 101
+    //throw error
+  })
+promise
+  .catch((error)=>{
+
+  })
+*/
+
+/*
+//chaining
+promise
+  .then((result)=>{
+    //Do Something
+    return 101;
+  })
+  .then((result)=>{
+    //result // 101
+    //throw error
+  })
+  .catch((error)=>{
+    
+  })
+*/
+/*
+let promise = new Promise(function(resolve, reject){
+  resolve(10);
+});
+
+//Calling the .then method multiple times
+//on a Single promise - It's not a chain
+promise.then(function(value){
+  value++;
+  return value;
+})
+promise.then(function(value){
+  value = value + 10;
+  return value;
+})
+promise.then(function(value){
+  value = value + 20;
+  console.log(value); //returns 30
+})
+*/
+
+//Handle Multiple Promises
+
+//promise.all([promises])
+
+const BULBASAUR_POKEMONS_URL = 'https://pokeapi.co/api/v2/pokemon/bulbasaur';
+const RATICATE_POKEMONS_URL = 'https://pokeapi.co/api/v2/pokemon/raticate';
+const KAKUNA_PIKEMONS_URL = 'https://pokeapi.co/api/v2/pokemon/kakuna';
+
+function getPromise(URL){
+  return fetch(URL)
+  .then((response) => {
+    if(!response.ok){
+      throw new Error(`HTTP error ${response.status}`);
+    }
+    return response.json();
+  })
+}
+
+let promise_1 = getPromise(BULBASAUR_POKEMONS_URL);
+let promise_2 = getPromise(RATICATE_POKEMONS_URL);
+let promise_3 = getPromise(KAKUNA_PIKEMONS_URL);
+/*
+Promise.all([promise_1, promise_2, promise_3])
+.then((result)=>{
+  console.log(result);
+})
+.catch((error)=>{
+  console.error(error);
+})
+*/
+//Doesn't wait for all the promises to resolve
+/*
+Promise.any([promise_1, promise_2, promise_3])
+.then((result)=>{
+  console.log(result);
+})
+.catch((error)=>{
+  console.error(error);
+})
+*/
+
+//settling a promise = fullfilling(resolve) + rejecting
+
+/*
+Promise.allSettled([promise_1, promise_2, promise_3])
+.then((result)=>{
+  console.log(result);
+})
+.catch((error)=>{
+  console.error(error);
+})
+*/
+
+Promise.race([promise_1, promise_2, promise_3])
+.then((result)=>{
+  console.log(result);
+})
+.catch((error)=>{
+  console.error('An error Occured');
+})
+
+Promise.resolve() //is same as
+let promise2 = new Promise(resolve => resolve(value));
+
+Promise.reject() // is same as
+let promise1 = new Promise((resolve, reject) => reject(error));
+
+
+//How to cancel a Promise
+//Promise cannot be cancelled only the process can be cancelled
